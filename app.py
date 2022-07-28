b1e04a6cc6b767355b9c4f6efded6b232d3ac29f
Le Pham <phamtle724@gmail.com>
2022-07-28 12:05:56 -0700

    fixed broken survey run brYan

--- app.py
+++ app.py
@@ -7,3 +7,37 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+
+# empty list to store the user's responses
+responses = []
+
+@app.get("/begin")
+def get_begin_survey():
+
+    return render_template("survey_start.html")
+
+
+
+
+
+
+@app.get("/questions")
+def show_questions():
+
+
+    return render_template("question.html")
+
+
+
+
+
+
+
+
+
+
+@app.get("/completion")
+def lets_open_completion():
+    """redirect to this after survey is completed"""
+    return render_template("completion.html")
